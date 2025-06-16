08b6240ed07e7c8b234cc80d71c43c3a693e46e9
Code Illusionists <illusionistscode@gmail.com>
2025-06-16 21:51:19 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // ✅ required!
 import path from 'path';
 
 export default defineConfig({
   root: 'client',
+  plugins: [react()], // ✅ enable React
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
